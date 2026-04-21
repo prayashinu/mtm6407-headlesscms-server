302c1c1ba39ffc6866ad2f9f1d99c55c487bd05b
prayash <Prayashkharel175@gmail.com>
2026-04-21 18:59:56 -0400

    Update admin.js

--- config/admin.js
+++ config/admin.js
@@ -1,20 +1,8 @@
-module.exports = ({ env }) => ({
-  auth: {
-    secret: env('ADMIN_JWT_SECRET'),
-  },
+export default ({ env }) => ({
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
-  transfer: {
-    token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
-    },
-  },
-  secrets: {
-    encryptionKey: env('ENCRYPTION_KEY'),
-  },
-  flags: {
-    nps: env.bool('FLAG_NPS', true),
-    promoteEE: env.bool('FLAG_PROMOTE_EE', true),
+  auth: {
+    secret: env('ADMIN_JWT_SECRET'),
   },
 });
